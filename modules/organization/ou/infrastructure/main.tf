99c3c7a4657222bdaf4dfe6e9acb797c2b99d68c
Guillaume GC <gc.commit@pm.me>
2025-04-21 15:24:09 +0200

    chore(orga): rename infra acc

--- modules/organization/ou/infrastructure/main.tf
+++ modules/organization/ou/infrastructure/main.tf
@@ -13,5 +13,5 @@ module "identity_account" {
   source = "../../account/default"
 
   parent_id = aws_organizations_organizational_unit.infrastructure.id
-  name      = "identity"
+  name      = "Identity"
 }
