baa0c47836eeab384d93060842ecef31cb82923d
Guillaume GC <gc.commit@pm.me>
2025-07-28 13:45:58 +0200

    feat(identity): move to organization acc

--- modules/organization/factory/identity/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-  alias  = "infrastructure"
-
-  assume_role {
-    role_arn    = "arn:aws:iam::${var.identity_account_id}:role/identity-root-role"
-    external_id = "managemenent-identity-root-role"
-  }
-}
--- modules/organization/ou/infrastructure/identity.tf
+++ modules/organization/ou/infrastructure/identity.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  management_account_id = data.aws_caller_identity.current.account_id
+}
+
 module "identity_account" {
   source = "../../template/account"
 
@@ -5,8 +11,39 @@ module "identity_account" {
   parent_id = aws_organizations_organizational_unit.infrastructure.id
 }
 
-# module "identity_account_factory" {
-#   source = "../../factory/identity"
-#
-#   identity_account_id = module.identity_account.account_id
-# }
+module "aws-iam-identity-center" {
+  source = "aws-ia/iam-identity-center/aws"
+
+  sso_groups = {
+    Admin : {
+      group_name        = "Admin"
+      group_description = "Admin IAM Identity Center Group."
+    }
+  }
+
+  permission_sets = {
+    AdministratorAccess = {
+      description          = "Provides AWS full access permissions.",
+      session_duration     = "PT6H", // How long until session expires - PT6H means 6 hours. Max is 12 hours.
+      aws_managed_policies = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+    }
+
+    ViewOnlyAccess = {
+      description          = "Provides AWS view only permissions.",
+      session_duration     = "PT6H",
+      aws_managed_policies = ["arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"]
+    }
+  }
+
+  account_assignments = {
+    Admin : {
+      principal_name  = "Admin"
+      principal_type  = "GROUP"
+      principal_idp   = "INTERNAL" // type of Identity Provider you are using. Valid values are "INTERNAL" (using Identity Store) or "EXTERNAL" (using external IdP such as EntraID, Okta, Google, etc.)
+      permission_sets = ["AdministratorAccess", "ViewOnlyAccess"]
+      account_ids = [
+        local.management_account_id,
+      ]
+    },
+  }
+}
