d114a948ce70a6af73acfdd5045fe191a05ba571
Guillaume GC <gc.commit@pm.me>
2025-07-28 10:35:18 +0200

    chore(orga): add random seed to avoid EMAIL_ALREADY_EXISTS error

--- modules/organization/account/default/main.tf
+++ modules/organization/account/default/main.tf
@@ -1,8 +1,17 @@
+resource "random_id" "email_seed" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ami_id = var.name
+  }
+
+  byte_length = 4
+}
+
 resource "aws_organizations_account" "default_account" {
   parent_id = var.parent_id
 
   name  = var.name
-  email = "org.acc+${lower(var.name)}@pm.me"
+  email = "org.acc+${lower(var.name)}-${random_id.email_seed.hex}@pm.me"
 
   role_name = "${lower(var.name)}-root-role"
 
