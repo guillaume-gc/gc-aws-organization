915355c231885f0547b4138a927e39c4565388a6
Guillaume GC <gc.commit@pm.me>
2025-07-28 13:49:53 +0200

    chore(providers): add default tags

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 provider "aws" {
   region = var.aws_default_region
   alias  = "management"
+
+  default_tags {
+    tags = {
+      GitBranch = var.git_branch_name
+      Service   = var.resources_prefix
+      ManagedBy = "Terraform"
+    }
+  }
 }
