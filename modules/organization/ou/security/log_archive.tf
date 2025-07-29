6469af909feed1e02ea7697a54e3adcd0ff2ba94
Guillaume GC <gc.commit@pm.me>
2025-07-29 16:37:34 +0200

    fix(security): account log archive name

--- modules/organization/ou/security/log_archive.tf
+++ modules/organization/ou/security/log_archive.tf
@@ -1,6 +1,6 @@
 module "log_archive_account" {
   source = "../../template/account"
 
-  name      = "Log_Archive"
+  name      = "Log-Archive"
   parent_id = aws_organizations_organizational_unit.security.id
 }
