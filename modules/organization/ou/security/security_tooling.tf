64bb8e13e9029c9d62c3faa73206adc722baa80c
Guillaume GC <gc.commit@pm.me>
2025-08-02 15:51:21 +0200

    feat(security_tooling): make acc delegated admin for cloudtrail

--- modules/organization/ou/security/security_tooling.tf
+++ modules/organization/ou/security/security_tooling.tf
@@ -4,3 +4,7 @@ module "security_tooling_account" {
   name      = "Security-Tooling"
   parent_id = aws_organizations_organizational_unit.security.id
 }
+
+resource "aws_cloudtrail_organization_delegated_admin_account" "delegated_admin_account" {
+  account_id = module.security_tooling_account.account_id
+}
