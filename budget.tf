106b65c27781a1e382a4261fb5d635395fd98edb
Guillaume GC <gc.commit@pm.me>
2025-08-06 19:35:54 +0200

    fix(alert): typo

--- budget.tf
+++ budget.tf
@@ -9,7 +9,7 @@ resource "aws_budgets_budget" "budget" {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 100
     threshold_type             = "PERCENTAGE"
-    notification_type          = "FORCASTED"
+    notification_type          = "FORECASTED"
     subscriber_email_addresses = var.notification_emails
   }
 
