a17935f833c53c79a6c81f9060468ee44bb70147
Guillaume GC <gc.commit@pm.me>
2025-08-06 19:38:50 +0200

    chore(account) add imports

--- organization.tf
+++ organization.tf
@@ -3,6 +3,16 @@ import {
   id = "o-iwno6geb2w"
 }
 
+import {
+  to = aws_organizations_organizational_unit.security
+  id = "ou-ixhm-1sf3rpz1"
+}
+
+import {
+  to = aws_organizations_organizational_unit.infrastructure
+  id = "ou-ixhm-0qvmcx39"
+}
+
 resource "aws_organizations_organization" "organization" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
