dc8296a474d618fc2ae7992ea9deec899d30c5f9
Guillaume GC <gc.commit@pm.me>
2025-08-06 20:37:26 +0200

    feat(explorer): add orga view

--- resource_explorer.tf
+++ resource_explorer.tf
@@ -1,3 +1,11 @@
 resource "aws_resourceexplorer2_index" "global_aggregator" {
   type = "AGGREGATOR"
 }
+
+resource "aws_resourceexplorer2_view" "organizational" {
+  name = "OrganizationalView"
+
+  scope = aws_organizations_organization.organization.id
+
+  depends_on = [aws_resourceexplorer2_index.global_aggregator]
+}
